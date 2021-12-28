3d7294091ad2fafac5d5d638f781fd1bb879e011
kajava <kajavasamuel@gmail.com>
2021-12-28 22:32:54 +0100

    Binary Search: find first occurence.
    It looks for the first occurence of an element since that is our most
    common use case :-)

--- courses/tda417/searching_sorting/BinarySearch.java
+++ courses/tda417/searching_sorting/BinarySearch.java
@@ -1,18 +1,22 @@
 public class BinarySearch {
     public static int search(int[] a, int goal) {
         int lo = 0;
-        int hi = a.length;
-        int mid = (lo + hi) / 2;
+        int hi = a.length-1;
+        int mid;
+        int index = -1;
 
         while (lo <= hi) {
+            mid = (lo + hi) / 2;
             if (a[mid] < goal)
                 lo = mid + 1;
             else if (a[mid] > goal)
                 hi = mid - 1;
-            else 
-                return mid;
+            else {
+                index = mid;
+                hi = mid - 1; // we want to find the first occurrence :^)
+            }
         }
 
-        return -1;
+        return index;
     }
 }
